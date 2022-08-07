385d2b9c433e3d7fd9daf5bf71e20917197d1e1b
Wesley Ramos <wesleyramox258@gmail.com>
2022-08-07 18:20:31 -0300

    Updates

--- pagination.js
+++ pagination.js
@@ -1,12 +1,11 @@
 
 function openPage(features) {
     var i;
-    var x = document.getElementsByClassName("city");
+    var x = document.getElementsByClassName("option");
 
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
     
     document.getElementById(features).style.display = "block";
-
 } 
